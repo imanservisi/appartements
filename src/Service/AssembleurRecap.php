160a1011937f8d1e13308a5da7d815e0a6dfca96
imanservisi <popovicsisabelle@gmail.com>
2025-11-22 17:04:40 +0100

    modification ordre récapitulatif

--- src/Service/AssembleurRecap.php
+++ src/Service/AssembleurRecap.php
@@ -15,19 +15,19 @@ class AssembleurRecap {
         }
         // mapping libellé => getter sur l'entité Recapitulatif
         $lignes = [
-            '210 RECETTES' => 'getTotalRecette',
-            '211 Loyers' => 'getLoyer',
+            'Loyers' => 'getLoyer',
+            'Total recettes' => 'getTotalRecette',
             'Frais administratifs' => 'getFraisAdm',
             'Autres frais' => 'getAutresFrais',
             'Primes assurances' => 'getPrimesAssurances',
             'Travaux' => 'getTravaux',
             'Taxe foncière' => 'getTaxeFonciere',
-            'Provision pour charges' => 'getProvisionPourCharge',
-            'Intérêts emprunt' => 'getInteretEmprunt',
-            'Montant 261' => 'getMontant261',
             'Montant 229bis' => 'getMontant229bis',
             'Montant 230' => 'getMontant230',
             'Montant 230bis' => 'getMontant230bis',
+            'Total charges' => 'getProvisionPourCharge',
+            'Intérêts emprunt' => 'getInteretEmprunt',
+            'Montant 261' => 'getMontant261',
         ];
         // initialiser les lignes avec des valeurs null pour chaque résidence
         $recapitulatifs = [];
